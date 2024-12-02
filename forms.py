00f90dba2749951989fb948ddb0d0ffe00442b0e
demonomed85 <politeh@mail.ru>
2024-12-02 22:28:59 +0300

    1

--- forms.py
+++ forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Length, Email
 
 class EditProfileForm(FlaskForm):
     username = StringField('Имя', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email')
     password = PasswordField('Пароль', validators=[DataRequired()])
     submit = SubmitField('Сохранить изменения')
 
@@ -13,3 +13,4 @@ class AddUserForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email(), Length(max=150)])
     password = PasswordField('Пароль', validators=[DataRequired(), Length(min=6)])
     submit = SubmitField('Добавить пользователя')
+
